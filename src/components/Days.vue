f10043290681cd5b0335c87563f09ed5f62275bc
Musatov Yuriy <colorkid@yandex.ru>
2018-03-26 08:02:45 +0300

    Update Days.vue

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="days">
-    <div class="day">
-      <div class="day__name">{{dayRender[0]}}</div>
-      <div class="day__room">
-        <div class="day__time"></div>
+    <div class="day" v-for="day in dayRender">
+      <div class="day__name">{{day[0]}}</div>
+      <div class="day__room" v-for="(room, typeRoom) in day[1]">
+        <div 
+        class="day__time" 
+        v-bind:data-value="time" 
+        v-bind:class="'day__time--' + time"
+        v-bind:data-way="[key, typeRoom,day[0]]" 
+        v-for="(time, key) in room" 
+        v-on:click="changeValueRoom">{{key}}
+      </div>
       </div>
     </div>
   </div>
@@ -11,23 +18,76 @@
 <script>
 export default {
   name: 'Days',
-  props: ['dayRender']
+  props: ['dayRender'],
+  methods: {
+    changeValueRoom: function() {
+      let way = event.target.dataset.way;
+      this.$emit('changevalueroom', way);
+    }
+  }
 }
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.days {
+  display: flex;
+  justify-content: space-between;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.day {
+  width: 20%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.day__name {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  border-right: 1px solid #eaeaea;
+  font-size: 15px;
 }
-a {
-  color: #42b983;
+
+.day:first-child .day__name{
+  border-left: 1px solid #eaeaea;
+}
+
+.day__room {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100px;
+  margin: 0 0 10px 0;
+  border: 1px solid #eaeaea;
+  border-left: none;
+  box-sizing: border-box;
+}
+
+.day__time {
+  display: flex;
+  align-items: center;
+  width: calc(50% - 10px);
+  padding: 0px 5px;
+  font-size: 12px;
+  position: relative;
+  cursor: pointer;
+}
+
+.day__time--free {
+  background-color: #fff;
+}
+
+.day__time--false {
+  background-color: #eaeaea;
+}
+
+.day__time:after {
+  content: "+";
+  position: absolute;
+  top: 1px;
+  right: 20px;
+  font-size: 14px;
+}
+
+.day__time:hover {
+  background-color: #f6f6f6;
 }
 </style>
